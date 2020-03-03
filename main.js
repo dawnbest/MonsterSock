0a872613487fd8bb04825dcfcdc1eec24e7d53d0
Dawn Best <55746955+dawnbest@users.noreply.github.com>
2020-03-03 09:10:35 -0500

    Add files via upload
    Made changes to the project's skeleton.

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-gsap.to(".monster", {duration: 2, xPercent: -50, ease: 'Elastic.easeInOut'});
-gsap.from("main", {opacity: 0, duration: 1, y:-50});
+gsap.to(".monster", {duration: 2, xPercent: -25, ease: 'Elastic.easeInOut'});
+gsap.from(".monban", {duration: 2, x:-100, ease: 'Bounce.out'});
+gsap.from("main", {opacity: 0, duration: 1, yPercent:-50});
