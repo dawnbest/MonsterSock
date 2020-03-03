436deb636e4cf2a95428024293bf7ed850ead2f3
Dawn Best <55746955+dawnbest@users.noreply.github.com>
2020-03-03 07:46:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 gsap.to(".monster", {duration: 2, xPercent: -50, ease: 'Elastic.easeInOut'});
-gsap.to("main", {opacity: 0, duration: 1, y:-50});
+gsap.from("main", {opacity: 0, duration: 1, y:-50});
